8434304ba33034355866dc5e5a7a96b1af0d3d00
nguyen van the <nguyenvanthe12h@gmail.com>
2022-06-07 10:39:20 +0700

    fix icon cờ bị vỡ

--- app/src/main/java/com/example/musicdownloader/adapter/SeeAllBinding.kt
+++ app/src/main/java/com/example/musicdownloader/adapter/SeeAllBinding.kt
@@ -1,5 +1,6 @@
 package com.example.musicdownloader.adapter
 
+import android.graphics.Color
 import com.example.musicdownloader.databinding.ItemTopListenedBinding
 import com.example.musicdownloader.interfaces.itemclickinterface.ItemClickListener
 import com.example.musicdownloader.interfaces.recyclerbindinginterface.RecyclerBindingInterface
@@ -17,6 +18,18 @@ object SeeAllBinding: RecyclerBindingInterface<ItemTopListenedBinding, Music> {
     ) {
         binder.music = model
         binder.tvId.text = (position + 1).toString() + "."
+        when(position){
+            0 ->{
+                binder.tvId.setTextColor(Color.parseColor("#F89500"))
+            }
+            1 ->{
+                binder.tvId.setTextColor(Color.parseColor("#00FF57"))
+            }
+            2 ->{
+                binder.tvId.setTextColor(Color.parseColor("#FF1C1C"))
+            }
+            else ->  binder.tvId.setTextColor(Color.parseColor("#FFFFFF"))
+        }
 
         binder.layoutTopListened.setOnClickListener{
             itemListener.onClickListener(model)
--- app/src/main/java/com/example/musicdownloader/viewmodel/ChangeRegionViewModel.kt
+++ app/src/main/java/com/example/musicdownloader/viewmodel/ChangeRegionViewModel.kt
@@ -17,17 +17,11 @@ class ChangeRegionViewModel: ViewModel(){
     private fun initRegions(){
         _regions.add(Region("us", "US", R.drawable.united_states))
         _regions.add(Region("gb", "UK", R.drawable.united_kingdom))
-        _regions.add(Region("za", "South Africa", R.drawable.south_africa))
-        _regions.add(Region("in", "India", R.drawable.india))
-        _regions.add(Region("ae", "UAE", R.drawable.united_arab_emirates))
         _regions.add(Region("au", "Australia", R.drawable.australia))
         _regions.add(Region("kr", "Korea", R.drawable.south_korea))
         _regions.add(Region("de", "Germany", R.drawable.germany))
         _regions.add(Region("jp", "Japan", R.drawable.japan))
-        _regions.add(Region("ph", "Philippines", R.drawable.philippines))
-        _regions.add(Region("ng", "Nigeria", R.drawable.nigeria))
-        _regions.add(Region("ke", "Kenya", R.drawable.kenya))
         _regions.add(Region("vn", "Việt Nam", R.drawable.vietnam))
-        _regions.add(Region("id", "Indonesia", R.drawable.indonesia))
+
     }
 }
